30233d21e0b9a7492c2421435cca62555d3e08b6
Sviatoslav <sviatoslav.k@nerdysoft.com>
2021-09-29 11:37:30 +0300

    Configure express server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
-const message: string = 'This is index.ts file';
+import express, { Express, Request, Response } from 'express';
+import { json, urlencoded } from 'body-parser';
+import dotenv from 'dotenv';
 
-const l = 1;
+dotenv.config();
 
-console.log({ message, l });
+const PORT = process.env.PORT || 3003;
+const ENV = process.env.NODE_ENV || 'development';
+const app: Express = express();
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>Hello from the TypeScript world!</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(
+    '\x1b[32m',
+    `Running on port http://localhost:${PORT} in ${ENV} mode⚡`,
+    '\x1b[37m',
+  );
+});
